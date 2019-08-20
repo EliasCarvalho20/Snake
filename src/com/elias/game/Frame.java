fca73d694a7aa445ec12db08dff1d950f25053c8
Elias Carvalho <eliascarvalho01@hotmail.com>
2019-08-20 12:34:47 -0300

    JFrame created

--- src/com/elias/game/Frame.java
+++ src/com/elias/game/Frame.java
@@ -1,9 +1,30 @@
 package com.elias.game;
 
-public class Frame {
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+	
+	public Frame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Snake");
+		setResizable(false);
+		
+		init();
+	}
+	
+	public void init() {
+		setLayout(new GridLayout(1, 1, 0, 0));	
+		
+		pack();
+		
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
 
 	public static void main(String[] args) {
-
+		new Frame();
 	}
 
 }
